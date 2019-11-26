bb62e28641d98088e084c5a34712bffff3ca70fc
rosendopili <rosendopili@gmail.com>
2019-11-26 17:24:21 -0500

    added a sound file that plays a sound effect when the ball hits the paddles and the top/bottom walls

--- Pong.py
+++ Pong.py
@@ -1,4 +1,5 @@
 import turtle
+import os
 
 win = turtle.Screen()
 # sets variable name for the screen
@@ -103,10 +104,12 @@ while True:
     if ball.ycor() > 290:
         ball.sety(290)
         ball.dy *= -1
+        os.system("afplay pong.wav")
 
     if ball.ycor() < -290:
         ball.sety(-290)
         ball.dy *= -1
+        os.system("afplay pong.wav")
     # making the ball interact with the top and & bottom borders
 
     if ball.xcor() > 390:
@@ -127,10 +130,12 @@ while True:
     if (ball.xcor() > 340 and ball.xcor() < 350) and (ball.ycor() < pad_b.ycor() + 40 and ball.ycor() > pad_b.ycor() -40):
         ball.setx(340)
         ball.dx *= -1
+        os.system("afplay pong.wav")
 
     if (ball.xcor() < -340 and ball.xcor() > -350) and (ball.ycor() < pad_a.ycor() + 40 and ball.ycor() > pad_a.ycor() -40):
         ball.setx(-340)
         ball.dx *= -1
+        os.system("afplay pong.wav")
 
 
 
